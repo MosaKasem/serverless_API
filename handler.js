7289f9b88576dd28f9ef6c2290ed49b0b28711a5
MosaKasem <thescorpion119@gmail.com>
2019-03-28 16:27:06 +0100

    initial commit

---
+++ handler.js
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports.endpoint = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, the current time is ${new Date().toTimeString()}.`
+    })
+  }
+
+  callback(null, response)
+}
+
+module.exports.imageResize = (event, context, callback) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: `Hello, the current time is ${new Date().toTimeString()}.`
+    })
+  }
+
+  callback(null, response)
+}
